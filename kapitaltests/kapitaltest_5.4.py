dc291c32b02160b08e373d8b44b2dbb8d73371c0
usirin01 <usirin2021@gmail.com>
2025-07-17 09:09:15 +0200

    kapitaltest_5.4 wurde geloest

---
+++ kapitaltests/kapitaltest_5.4.py
@@ -0,0 +1,26 @@
+import math
+
+print("Willkomen beim TIP-Löser für quadrische Gleichungen!")
+
+#Eingabe des Benutzers
+a = float(input("Gib einen Wert für a ein: "))
+b = float(input("Gib einen Wert für b ein: "))
+c = float(input("Gib einen Wert für c ein: "))
+
+#Eingaben anzeigen
+print("Deine Eingaben: ")
+print("a = " + str(a) + ", b = " + str(b) + ", c = " + str(c))
+
+#Berechnung der Diskriminante
+D = b ** 2 - 4 * a * c
+D = round(D, 4)  # Runden auf vier Dezimalstellen
+
+#Lösung
+x1 = (-b + math.sqrt(D)) / (2 * a)
+x2 = (-b - math.sqrt(D)) / (2 * a)
+
+#Ergebnis anzeigen
+print("Daraus ergeben sich folgende Lösungen: ")
+print("Diskriminante D = ")
+print("Lösung x_1 = " + str(x1))
+print("Lösung x_1 = " + str(x2))
