fa74d25c9aeb7b347bf213a6242a6f6ef67ab4c5
usirin01 <usirin2021@gmail.com>
2025-07-22 11:21:29 +0200

    aufgabe_6.2 abgeschlossen

---
+++ themenaufgaben/themenaufgaben_6.2.py
@@ -0,0 +1,11 @@
+# Liste der diesen Aufgabe
+
+my_numbers = [3.5, 4.5, 25.5, 40.5]
+
+# Berechnung
+
+resultat = ((my_numbers[2] * my_numbers[0]) - my_numbers[0] ** 2 ) / (my_numbers[3] - my_numbers[1] )
+
+# Ausgabe des Ergebnisses
+
+print(resultat)
