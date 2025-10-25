46f34c1a9ff3972f47ca14846a9996befd49094a
usirin01 <usirin2021@gmail.com>
2025-10-25 21:01:41 +0200

    themenaufgabe_7.4_1 ist fertig.

---
+++ themenaufgaben/themenaufgabe_7.4_1.py
@@ -0,0 +1,26 @@
+hauptstaedte = ["Bern", "Berlin", "Paris", "Rom", "Madrid", "Ankara"]
+
+#Eingabe eins
+
+stadt1 = input("Gib mir eine Stadtnamen: ")
+if stadt1 in hauptstaedte:
+    print(stadt1 + " ist eine Hauptstadt.")
+else:
+    print("Ich weiss leider nicht, ob " + stadt1 + " eine Hauptstadt ist.")
+
+#Eingabe zwei
+
+stadt2 = input("Gib mir eine Stadtnamen: ")
+if stadt2 in hauptstaedte:
+    print(stadt2 + " ist eine Hauptstadt.")
+else:
+    print("Ich weiss leider nicht, ob " + stadt2 + " eine Hauptstadt ist.")
+
+#Eingabe drei
+
+stadt3 = input("Gib mir eine Stadtnamen: ")
+if stadt3 in hauptstaedte:
+    print(stadt3 + " ist eine Hauptstadt.")
+else:
+    print("Ich weiss leider nicht, ob " + stadt3 + " eine Hauptstadt ist.")
+
